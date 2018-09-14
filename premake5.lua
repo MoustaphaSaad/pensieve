1f7ccdb3ae5fe1490106eeb0fae622298b80e222
Moustapha Saad <moustapha.saad.abdelhamed@gmail.com>
2018-09-14 14:13:57 +0200

    add msgpack

--- premake5.lua
+++ premake5.lua
@@ -11,6 +11,7 @@ end
 bin_path 		= path.getabsolute("bin")
 build_path 		= path.getabsolute("build")
 cpprelude_path 	= path.getabsolute("external/cpprelude/cpprelude")
+msgpack_path	= path.getabsolute("external/msgpack-c")
 pensieve_path	= path.getabsolute("pensieve")
 
 workspace "pensieve"
@@ -20,5 +21,6 @@ workspace "pensieve"
 	startproject "scratch"
 
 	include (cpprelude_path .. "/cpprelude.lua")
+	include (msgpack_path .. "/msgpack-c.lua")
 	include ("pensieve/pensieve.lua")
 	include ("scratch/scratch.lua")
